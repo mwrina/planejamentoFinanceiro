964638dac6fa47697ff97d7ce8b06bc07e6de2a4
mari <95420095+mwrina@users.noreply.github.com>
2025-05-02 10:51:04 -0400

    Delete backend directory

--- backend/routes/usuario_routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { criarUsuario } from '../controllers/usuario_controller.js';
-
-const router = express.Router();
-
-router.post('/usuarios', criarUsuario);
-router.post('/login', loginUsuario);
-
-export default router;
